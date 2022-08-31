0fbe209e8ce3227cc29527728c1f75c2c54cb1db
Serge-Andre-MASSON <serge.andre.masson@gmail.com>
2022-08-31 09:03:16 +0200

    First working poc

---
+++ data_access/data_access.py
@@ -0,0 +1,32 @@
+from io import BytesIO
+from PIL import Image
+import pickle
+
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import storage
+
+
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = storage.Client(credentials=credentials)
+
+bucket_name = "pyblood_bucket"
+
+
+def get_image(img_path):
+    """Return the image located at img_path on the a google cloud storage."""
+
+    bucket = client.bucket(bucket_name)
+    img_as_bytes = bucket.blob(img_path).download_as_bytes()
+
+    return Image.open(BytesIO(img_as_bytes))
+
+
+def get_dataset_infos():
+    """Return a DataFrame containing path , width, height and cell's type for each image."""
+
+    with open('data_access/PBC_infos.PICKLE', 'rb') as f:
+        PBC_infos_df = pickle.load(f)
+    return PBC_infos_df
