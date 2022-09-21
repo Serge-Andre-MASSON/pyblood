76e8a5ce81278c6be73f47b2bb49294f0e63cb48
Serge <serge.andre.masson@gmail.com>
2022-09-21 06:50:13 +0200

    Implement remote data updating

---
+++ data_access/update_data.py
@@ -0,0 +1,48 @@
+import math
+from tqdm import tqdm
+import os
+from google.cloud import storage
+from google.oauth2 import service_account
+
+
+credentials = service_account.Credentials.from_service_account_file(
+    ".streamlit/pyblood-16bca61a00f2.json")
+client = storage.Client(credentials=credentials)
+
+BUCKET_NAME = "pyblood_bucket"
+BUCKET = client.bucket(BUCKET_NAME)
+DATA_ACCESS = 'google clood'
+
+
+def upload_files(*file_paths):
+    for file_path in tqdm(file_paths):
+        blob = BUCKET.blob(file_path)
+        blob.upload_from_filename(file_path)
+
+
+def download_files(*file_paths):
+    for file_path in tqdm(file_paths):
+        blob = BUCKET.blob(file_path)
+        blob.download_to_filename(file_path)
+
+
+ROOT_URL = "https://storage.googleapis.com/pyblood_bucket/"
+
+
+def upload_data():
+    for root, dirnames, files in os.walk("data/"):
+        if files and "PBC_dataset_normal_DIB" not in root:
+            for file in files:
+                file_path = root + "/" + file
+                blob = BUCKET.blob(file_path)
+                if not blob.exists():
+                    print("Uploading :", file_path)
+                    blob.upload_from_filename(file_path, timeout=4000)
+
+
+def download_data():
+    blobs = client.list_blobs(BUCKET)
+    for blob in blobs:
+        blob_name = blob.public_url.replace(ROOT_URL, "")
+        if not os.path.exists(blob_name):
+            BUCKET.blob(blob_name).download_to_filename(blob_name)
