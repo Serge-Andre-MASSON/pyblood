c166816e99d3f270e1f5068acd358fa1a5628c2b
Serge <serge.andre.masson@gmail.com>
2022-09-28 10:25:25 +0200

    Réparation des chemins d'accès aux figures

--- pages/04_Presentation_des_reseaux_de_neurones.py
+++ pages/04_Presentation_des_reseaux_de_neurones.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 from data_access.data_access import load_pickle
-from data_access.data_paths import get_pbc_dataset_infos_paths
+from data_access.data_paths import get_figure_path, get_pbc_dataset_infos_paths
 from keras.applications.vgg16 import VGG16
 from keras.layers.preprocessing.image_preprocessing import Rescaling
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D, Dropout, Conv2D, MaxPooling2D, BatchNormalization, LeakyReLU, Flatten
@@ -13,58 +13,41 @@ st.sidebar.markdown("# Présentation des CNN utilisés")
 st.sidebar.write("""Dans cette partie, nous présentons les caractéristiques des divers réseaux de neurones testés ainsi, que leurs performances.
 Les résultats présentés sont ceux des transfer learning effectués à partir de chacun des modèles choisis. """)
 
+
 def section_1():
     st.header("Modèle de transfer learning avec VGG16")
     st.subheader("Paramètres du modèle et résultats")
-    st.write("\n")
-    st.write("\n")
-    st.write("\n")
-    st.write("\n")
-    #img_shape=(256,256,3)
-    #vgg16=VGG16(input_shape=img_shape, weights="imagenet", include_top=False)
-    #for layer in vgg16.layers[-4:]:
-        #layer.trainable = True
-    #normalization_layer = Rescaling(1./255, input_shape=img_shape)
-    #model_vgg16=Sequential()
-    #model_vgg16.add(normalization_layer)
-    #model_vgg16.add(vgg16)
-    #model_vgg16.add(Dense(500, activation='relu'))
-    #model_vgg16.add(Dropout(0.5))
-    #model_vgg16.add(Dense(250, activation='relu'))
-    #model_vgg16.add(Dropout(0.5))
-    #model_vgg16.add(Flatten())
-    #model_vgg16.add(Dense(8, activation='softmax'))
-    #model_vgg16.summary(print_fn=lambda x: st.text(x))
-    
+    st.write(4*"\n")
+
     # Display Images
- 
+
     # import Image from pillow to open images
     from PIL import Image
     # chemin d'accès aux figures
-    dir_img="D:\Bureau\FORMATION DATASCIENCE\PROJET\pyblood\data\\figures"
+    figure_path = get_figure_path("vgg16_summary")
     # image du résumé du modèle vgg16
-    img = Image.open(dir_img + "\\vgg16_summary.jpg")
- 
+    img = Image.open(figure_path)
+
     # display image using streamlit
     # width is used to set the width of an image
     st.image(img, width=800)
 
     st.write("L’entraînement du modèle est effectué sur seulement 10 epochs mais se stoppe à 6 epochs car l’accuracy n’évolue plus et nous obtenons une accuracy de 95% contre 94% sans fine-tuning avec un temps d’entraînement d’environ 10 minutes. En revanche, nous remarquons un léger overfitting avec ce modèle")
-   
+
     # image du rapport de classification vgg16
-    img_2=Image.open(dir_img+"\\rapport_classification_vgg16.jpg")
+    # chemin d'accès aux figures
+    figure_path = get_figure_path("rapport_classification_vgg16")
+    # image du résumé du modèle vgg16
+    img_2 = Image.open(figure_path)
     col1, col2, col3 = st.columns([0.2, 1, 0.2])
     col2.image(img_2, use_column_width=True, width=200)
-    
-    
-    # image de la courbe accuracy de vgg16
-    img_3=Image.open(dir_img+'\\courbe_vgg16.jpg')
-    col1, col2, col3 = st.columns([0.2, 0.5, 0.2])
-    col2.image(img_3, use_column_width=True, width=200)
-    
 
-   
-    
+    # image de la courbe accuracy de vgg16
+    img_3 = Image.open(get_figure_path("courbe_vgg16"))
+    _, col2, _ = st.columns([0.2, 0.5, 0.2])
+    col2.image(img_3, use_column_width=True, width=200)
+
+
 def section_2():
     st.header("Modèle de transfer learning avec InceptionV3")
     st.subheader("Paramètres du modèle et résultats")
@@ -74,20 +57,20 @@ def section_2():
     st.write("\n")
     from PIL import Image
     # image du résumé du modèle inceptionV3
-    dir_img="D:\Bureau\FORMATION DATASCIENCE\PROJET\pyblood\data\\figures"
-    img_4=Image.open(dir_img+'\\inceptionv3_summary.jpg')
+    img_4 = Image.open(get_figure_path("inceptionv3_summary"))
     st.image(img_4, width=800)
     st.write("L’entraînement du modèle est effectué sur seulement 10 epochs et nous obtenons une accuracy de 92% contre 90% sans fine-tuning avec un temps d’entraînement d’environ 10 minutes. En revanche, nous remarquons un overfitting assez important avec ce modèle.")
 
     # image du rapport de classification inceptionV3
-    img_5=Image.open(dir_img+"\\rapport_classification_inceptionv3.jpg")
-    col1, col2, col3 = st.columns([0.2, 1, 0.2])
+    img_5 = Image.open(get_figure_path("rapport_classification_inceptionv3"))
+    _, col2, _ = st.columns([0.2, 1, 0.2])
     col2.image(img_5, use_column_width=True, width=200)
 
     # image de la courbe accuracy de inceptionv3
-    img_6=Image.open(dir_img+'\\courbe_inceptionv3.jpg')
-    col1, col2, col3 = st.columns([0.2, 0.5, 0.2])
+    img_6 = Image.open(get_figure_path("courbe_inceptionv3"))
+    _, col2, _ = st.columns([0.2, 0.5, 0.2])
     col2.image(img_6, use_column_width=True, width=200)
+
 
 def section_3():
     st.header("Modèle de transfer learning avec Densenet121")
@@ -98,20 +81,20 @@ def section_3():
     st.write("\n")
     from PIL import Image
     # image du résumé du modèle Densenet121
-    dir_img="D:\Bureau\FORMATION DATASCIENCE\PROJET\pyblood\data\\figures"
-    img_4=Image.open(dir_img+'\\densenet_summary.jpg')
+    img_4 = Image.open(get_figure_path("densenet_summary"))
     st.image(img_4, width=800)
     st.write("L’entraînement du modèle est effectué sur seulement 25 epochs et nous obtenons une accuracy de 98% contre 95% sans fine-tuning avec un temps d’entraînement d’environ 50 minutes.")
 
     # image du rapport de classification Densenet121
-    img_5=Image.open(dir_img+"\\rapport_classification_densenet.jpg")
-    col1, col2, col3 = st.columns([0.2, 1, 0.2])
+    img_5 = Image.open(get_figure_path("rapport_classification_densenet"))
+    _, col2, _ = st.columns([0.2, 1, 0.2])
     col2.image(img_5, use_column_width=True, width=200)
 
     # image de la courbe accuracy de Densenet121
-    img_6=Image.open(dir_img+'\\courbe_densenet.jpg')
-    col1, col2, col3 = st.columns([0.2, 0.5, 0.2])
+    img_6 = Image.open(get_figure_path("courbe_densenet"))
+    _, col2, _ = st.columns([0.2, 0.5, 0.2])
     col2.image(img_6, use_column_width=True, width=200)
+
 
 def section_4():
     st.header("Modèle de transfer learning avec Basic Model")
@@ -122,20 +105,20 @@ def section_4():
     st.write("\n")
     from PIL import Image
     # image du résumé du modèle Basic Model
-    dir_img="D:\Bureau\FORMATION DATASCIENCE\PROJET\pyblood\data\\figures"
-    img_4=Image.open(dir_img+'\\basic_model_summary.jpg')
+    img_4 = Image.open(get_figure_path("basic_model_summary"))
     st.image(img_4, width=800)
     st.write("L’entraînement du modèle est effectué jusqu’à ce que l’accuracy de validation augmente de moins de 0.1% sur 10 epochs, avec également un mécanisme de réduction du taux d’apprentissage en cas de détection de plateau. Nous obtenons une accuracy de 96% avec un temps d’entraînement d’environ 30 minutes. Nous constatons que le phénomène de divergence précédemment observé se reproduit, mais plus tard, à partir de la 20ème epoch environ. De plus, il est moins prononcé, l’accuracy d’entraînement montant jusqu’aux alentours de 99% tandis que celle de validation plafonne autour de 96% à partir de la 25ème epoch environ.")
     # image du rapport de classification Basic Model
-    img_5=Image.open(dir_img+"\\rapport_classification_basic_model.jpg")
-    col1, col2, col3 = st.columns([0.2, 1, 0.2])
+    img_5 = Image.open(get_figure_path("rapport_classification_basic_model"))
+    _, col2, _ = st.columns([0.2, 1, 0.2])
     col2.image(img_5, use_column_width=True, width=200)
 
     # image de la courbe accuracy de Basic Model
-    img_6=Image.open(dir_img+'\\courbe_basic_model.jpg')
-    col1, col2, col3 = st.columns([0.2, 0.5, 0.2])
+    img_6 = Image.open(get_figure_path("courbe_basic_model"))
+    _, col2, _ = st.columns([0.2, 0.5, 0.2])
     col2.image(img_6, use_column_width=True, width=200)
-    
+
+
 page_names_to_funcs = {
     "VGG16": section_1,
     "InceptionV3": section_2,
