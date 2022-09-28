7eb7678e66e81f2cefd2916b42c6f4d37b90df65
Serge <serge.andre.masson@gmail.com>
2022-09-28 11:52:04 +0200

    Refactor dl demo and add ml_plot.py.

---
+++ data_viz/ml_plot.py
@@ -0,0 +1,32 @@
+from data_access.data_access import get_random_image, load_model
+from data_access.data_paths import get_dl_model_path
+import tensorflow as tf
+import streamlit as st
+import matplotlib.pyplot as plt
+
+
+CLASSES = ['basophil', 'eosinophil', 'erythroblast', 'ig',
+           'lymphocyte', 'monocyte', 'neutrophil', 'platelet']
+
+
+@st.experimental_memo
+def plot_predictions(counter):
+    img, target = get_random_image()
+    img = img.resize((256, 256))
+    tensor_img = tf.keras.utils.img_to_array(img).reshape(-1, 256, 256, 3)
+    model_path = get_dl_model_path('dense_net')
+    model = load_model(model_path)
+    prediction = model.predict(tensor_img)[0]
+
+    fig_1, ax = plt.subplots()
+    ax.imshow(img)
+    ax.set_title(target)
+    ax.set_axis_off()
+
+    fig_2, ax = plt.subplots()
+    ax.bar(CLASSES, height=prediction)
+    ax.set_title("Prévision")
+    ax.tick_params(axis='x', labelrotation=45)
+    correctness = "correcte et estimée" if target == CLASSES[
+        prediction.argmax()] else "incorrecte, bien qu'estimée"
+    return fig_1, fig_2, correctness, prediction
--- pages/05_Demo_deep_learning.py
+++ pages/05_Demo_deep_learning.py
@@ -1,43 +1,29 @@
 import streamlit as st
-from data_access.data_access import get_random_image, load_model
-from data_access.data_paths import get_dl_model_path
-import tensorflow as tf
-from data_viz.plot import reload_content
-import matplotlib.pyplot as plt
+from data_viz.ml_plot import plot_predictions
 
-CLASSES = ['basophil', 'eosinophil', 'erythroblast', 'ig',
-           'lymphocyte', 'monocyte', 'neutrophil', 'platelet']
+
+def init_session_state(*states):
+    for state in states:
+        if state not in st.session_state:
+            st.session_state[state] = 0
+
+
+def increment_counter(counter_state):
+    st.session_state[counter_state] += 1
 
 
 def images_from_original_dataset():
-    st.markdown("# Inférence sur les images du dataset original")
+
+    init_session_state('pred_counter')
+    st.markdown("# Dense Net 121")
+    st.markdown("## Inférence sur les images du dataset original")
     st.write("""Le modèle utilisé est le model dense net 121, initialisé avec les poids 'image_net',
     ajusté ensuite sur 80% du jeu de données. Les images choisies aléatoirement pour illustrer
     le fonctionnement du modèle proviennent indifférement du jeu d'entrainement ou du jeu de validation.""")
 
-    def plot_predictions():
-        img, target = get_random_image()
-        img = img.resize((256, 256))
-        tensor_img = tf.keras.utils.img_to_array(img).reshape(-1, 256, 256, 3)
-        model_path = get_dl_model_path('dense_net')
-        model = load_model(model_path)
-        prediction = model.predict(tensor_img)[0]
-
-        fig_1, ax = plt.subplots()
-        ax.imshow(img)
-        ax.set_title(target)
-        ax.set_axis_off()
-
-        fig_2, ax = plt.subplots()
-        ax.bar(CLASSES, height=prediction)
-        ax.set_title("Prévision")
-        ax.tick_params(axis='x', labelrotation=45)
-        correctness = "correcte et estimée" if target == CLASSES[
-            prediction.argmax()] else "incorrecte, bien qu'estimée"
-        return fig_1, fig_2, correctness, prediction
-
     col1, col2 = st.columns(2)
-    fig_1, fig_2, correctness, prediction = plot_predictions()
+    pred_counter = st.session_state['pred_counter']
+    fig_1, fig_2, correctness, prediction = plot_predictions(pred_counter)
     with col1:
         img_placeholder = st.empty()
         img_placeholder.pyplot(fig_1)
@@ -49,8 +35,9 @@ def images_from_original_dataset():
         st.write(
             f"La prediction est {correctness} comme probable à {prediction.max()*100:.0f}%.")
 
-    # Je n'ai pas encore trouvé la bonne méthode pour recharger un graphe...
-    st.button("reload", on_click=plot_predictions)
+    st.button("reload", on_click=increment_counter, args=('pred_counter',))
+
+    st.markdown("## Visualisation des erreurs")
 
 
 page_names_to_funcs = {
